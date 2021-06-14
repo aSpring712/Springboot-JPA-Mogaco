ba0f3056487ac4766d690d51f988203f854171fd
aSpring <rnjsqhal2001@naver.com>
2021-06-14 09:37:36 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/mogaco/MogacoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cos.mogaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogacoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogacoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/mogaco/MogacoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.mogaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogacoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
